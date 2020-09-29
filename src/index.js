bf9d50ca874ec88337b82a68909af0f887b170c3
melyaua <igor.melnychenko@tmx-learning.com>
2020-09-29 13:24:40 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0
+    }
+    return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0
+    }
+    return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length === 0) {
+        return 0
+    }
+    let total = 0;
+    let items = 0;
+    for (let i = 0; i < array.length; i++) {
+    total += array[i]
+    items += 1;
+    }
+    return total / items;
 }
